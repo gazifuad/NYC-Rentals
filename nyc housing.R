cf7a8779f3ed579f3ceb6e53419b62de306b7e30
gazifuad <gazi.fuad@gmail.com>
2022-07-01 00:42:19 -0400

    cleaning up data and new plots

--- nyc housing.R
+++ nyc housing.R
@@ -1,5 +1,9 @@
+# Import libraries
 library(tidyr)
+library(ggplot2)
+library(readr)
 
+# Read in data
 airbnb <- read.csv('airbnb_listings.csv')
 
 se_med_rent_studio <- read.csv('medianAskingRent_Studio.csv')
@@ -19,9 +23,20 @@ airbnb_subset <- subset(airbnb, select = c('id', 'listing_url', 'last_scraped',
                                            'minimum_nights', 'maximum_nights',
                                            "availability_365"))
 
-airbnb_subset <- subset(airbnb_subset, airbnb_subset$room_type == 'Entire home/apt')
+airbnb_subset <- subset(airbnb_subset, 
+                        airbnb_subset$room_type == 'Entire home/apt' & 
+                          airbnb_subset$bedrooms <= 2) %>% 
+  drop_na(bedrooms, bathrooms_text, beds, price)
 
-airbnb_subset <- airbnb_subset %>% drop_na(bedrooms, bathrooms_text, beds,
-                                           price)
+airbnb_subset$price <- parse_number(airbnb_subset$price)
 
-airbnb_subset <- subset(airbnb_subset, airbnb_subset$bedrooms <= 2)
+summary(airbnb_subset$minimum_nights)
+
+airbnb_subset_long_term <- subset(airbnb_subset, 
+                                  airbnb_subset$minimum_nights >= 28 &
+                                    airbnb_subset$availability_365 >= 28)
+
+  
+ggplot(data = airbnb_subset_long_term, aes(x=neighbourhood_cleansed, y = price)) +
+  geom_boxplot() + facet_wrap(vars(neighbourhood_group_cleansed), scales = "free") + 
+  theme(axis.text.x = element_text(angle = 90)) + coord_flip()
